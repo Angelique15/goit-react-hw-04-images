b73fd9e5b44f28ebbabeb72cad4ac8f8059e7139
Angelique15 <113044854+Angelique15@users.noreply.github.com>
2023-05-18 21:19:30 +0000

    Deploying to gh-pages from @ Angelique15/goit-react-hw-04-images@34111e1af8a309c0fa60c6c167a68a22868f46ea 🚀

--- src/styles/Gallery.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Gallery = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  list-style: none;
-  padding: 0;
-  background-color: #E0F4F2;
-`;
