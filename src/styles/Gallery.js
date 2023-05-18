bf83de1275099d6af64ee7d7837c998ec3fd4bba
Angelique15 <mar.angel.go15@gmail.com>
2023-05-18 16:08:50 -0500

    update hooks

---
+++ src/styles/Gallery.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Gallery = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  list-style: none;
+  padding: 0;
+  background-color: #E0F4F2;
+`;
